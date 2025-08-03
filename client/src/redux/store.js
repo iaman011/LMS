f3a76b90483dc433b13b734cc58fff4940ee2470
iaman011 <iaman.singh011@gmail.com>
2025-08-03 15:53:57 +0530

    setupe with redux toolkit done

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
+import authReducer from './slices/authSlice.js'
+
 
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        auth: authReducer
+    },
     devTools: true
-})
+});
+
+export default store;

c93cab10fce876a0753ff60a89116f2d41510ab2
iaman011 <iaman.singh011@gmail.com>
2025-08-26 11:13:14 +0530

    Denied paged added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Notfound from "./pages/Notfound";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Contact from "./pages/Contact";
+import Denied from "./pages/Denied";
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/contacts" element={<Contact />} />
+        <Route path="/denied" element={<Denied />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </div>
